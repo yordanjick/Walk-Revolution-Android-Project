19217c5e4fa64db2e21444e1705997d4feab3e68
Viktor <38926075+Viktoryyy@users.noreply.github.com>
2020-02-06 23:57:07 -0800

    Update CreateRouteActivity.java

--- app/src/main/java/com/example/cse110_project/CreateRouteActivity.java
+++ app/src/main/java/com/example/cse110_project/CreateRouteActivity.java
@@ -3,6 +3,7 @@ package com.example.cse110_project;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.room.Room;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.view.View;
@@ -33,24 +34,24 @@ public class CreateRouteActivity extends AppCompatActivity {
         final EditText step=findViewById(R.id.step_field);
 
         final RadioGroup runtype=findViewById(R.id.run_type);
-        RadioButton runtype1=findViewById(R.id.loop_button);
+        final RadioButton runtype1=findViewById(R.id.loop_button);
         RadioButton runtype2=findViewById(R.id.out_and_back_button);
 
         final RadioGroup f_h=findViewById(R.id.flat_hilly);
-        RadioButton f_h1=findViewById(R.id.flat_button);
+        final RadioButton f_h1=findViewById(R.id.flat_button);
         RadioButton f_h2=findViewById(R.id.hilly_button);
 
         final RadioGroup routetype=findViewById(R.id.route_type);
-        RadioButton routetype1=findViewById(R.id.streets_button);
+        final RadioButton routetype1=findViewById(R.id.streets_button);
         RadioButton routetype2=findViewById(R.id.trail_button);
 
         final RadioGroup surfacetype=findViewById(R.id.surface_type);
-        RadioButton surfacetype1=findViewById(R.id.even_button);
+        final RadioButton surfacetype1=findViewById(R.id.even_button);
         RadioButton surfacetype2=findViewById(R.id.uneven_button);
 
         final RadioGroup difficulity=findViewById(R.id.difficulty);
-        RadioButton difficulity1=findViewById(R.id.easy_button);
-        RadioButton difficulity2=findViewById(R.id.moderate_button);
+        final RadioButton difficulity1=findViewById(R.id.easy_button);
+        final RadioButton difficulity2=findViewById(R.id.moderate_button);
         RadioButton difficulity3=findViewById(R.id.hard_button);
 
         final MultiAutoCompleteTextView note=findViewById(R.id.note_field);
@@ -67,18 +68,32 @@ public class CreateRouteActivity extends AppCompatActivity {
                 else{
                     RouteEntry routeEntry=new RouteEntry(name.getText().toString(),start.getText().toString());
 
-                    routeEntry.setRun(runtype.getCheckedRadioButtonId());
-                    routeEntry.setTerrain(f_h.getCheckedRadioButtonId());
-                    routeEntry.setRoadType(routetype.getCheckedRadioButtonId());
-                    routeEntry.setRoadCondition(surfacetype.getCheckedRadioButtonId());
-                    routeEntry.setLevel(difficulity.getCheckedRadioButtonId());
+                    routeEntry.setRun(runtype.getCheckedRadioButtonId()==runtype1.getId()?0:1);
+                    routeEntry.setTerrain(f_h.getCheckedRadioButtonId()==f_h1.getId()?0:1);
+                    routeEntry.setRoadType(routetype.getCheckedRadioButtonId()==routetype1.getId()?0:1);
+                    routeEntry.setRoadCondition(surfacetype.getCheckedRadioButtonId()==surfacetype1.getId()?0:1);
+                    if(difficulity.getCheckedRadioButtonId()==difficulity1.getId()){
+                    routeEntry.setLevel(0);
+                    }
+                    else if(difficulity.getCheckedRadioButtonId()==difficulity2.getId()){
+                        routeEntry.setLevel(1);
+                    }
+                    else{
+                        routeEntry.setLevel(2);
+                    }
                     routeEntry.setNote(note.getText().toString());
                     dao.insertRoute(routeEntry);
+                    launchActivity();
                 }
             }
         });
 
 
 
+    }
+    //Switch to routes page
+    public void launchActivity(){
+        Intent intent=new Intent(this,RoutesListActivity.class);
+        startActivity(intent);
     }
 }
