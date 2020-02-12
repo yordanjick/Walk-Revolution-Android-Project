ad4f3ef32c53b5dbbaff4ea5bb3eb5affd124e6a
Viktor <38926075+Viktoryyy@users.noreply.github.com>
2020-02-12 00:31:25 -0800

    add lengthtransfer file

---
+++ app/src/main/java/com/example/cse110_project/LengthTransfer.java
@@ -0,0 +1,16 @@
+package com.example.cse110_project;
+
+public class LengthTransfer {
+    public LengthTransfer(){}
+
+    public String transfer(float length){
+
+
+
+
+
+        return "1";
+    }
+
+
+}
