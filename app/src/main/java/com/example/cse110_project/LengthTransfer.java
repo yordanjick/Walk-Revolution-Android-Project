1a46ed86d73075d3f20e47e047c2b29ad17ce9c1
Hackerry <harrynjc@yahoo.com>
2020-02-12 22:43:29 -0800

    @7 Make Lengthtransfer class static

--- app/src/main/java/com/example/cse110_project/LengthTransfer.java
+++ app/src/main/java/com/example/cse110_project/LengthTransfer.java
@@ -1,22 +1,17 @@
 package com.example.cse110_project;
 
-public class LengthTransfer {
-    public LengthTransfer(){}
+import java.util.Locale;
 
-    public String transfer(float length){
-        String str=String.format("%.2f",length);
-            if(length>=10000){
-                length=length/1000;
-                str=String.format("%.2f",length);
-                str+="k";
+public class LengthTransfer {
+    public static String transfer(float length){
+        String str=String.format(Locale.US, "%.2f", length);
+            if(length >= 10000){
+                length = length/1000;
+                str = String.format(Locale.US, "%.2f", length);
+                str += "k";
 
             }
 
-
-
-
             return str;
     }
-
-
 }
--- app/src/test/java/com/example/cse110_project/LengthTransferTest.java
+++ app/src/test/java/com/example/cse110_project/LengthTransferTest.java
@@ -5,27 +5,21 @@ import org.junit.Test;
 import static junit.framework.TestCase.assertEquals;
 
 public class LengthTransferTest {
-    public LengthTransfer lengthTransfer;
-    @Before
-    public void init(){
-        lengthTransfer=new LengthTransfer();
-    }
-
     @Test
     public void normalTest(){
-        assertEquals(lengthTransfer.transfer(121566),"121.57k");
+        assertEquals(LengthTransfer.transfer(121566),"121.57k");
     }
     @Test
     public void normalTest2(){
-        assertEquals(lengthTransfer.transfer(121564),"121.56k");
+        assertEquals(LengthTransfer.transfer(121564),"121.56k");
     }
     @Test
     public void normalTest3(){
-        assertEquals(lengthTransfer.transfer(121),"121.00");
+        assertEquals(LengthTransfer.transfer(121),"121.00");
     }
     @Test
     public void cornerTest(){
-        assertEquals(lengthTransfer.transfer(0),"0.00");
+        assertEquals(LengthTransfer.transfer(0),"0.00");
     }
 
 
