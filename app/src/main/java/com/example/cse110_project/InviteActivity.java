efa5e8825ab1de995d1afc2ae916026b0d360422
Viktor <38926075+Viktoryyy@users.noreply.github.com>
2020-02-29 23:13:48 -0800

    add

--- app/src/main/java/com/example/cse110_project/InviteActivity.java
+++ app/src/main/java/com/example/cse110_project/InviteActivity.java
@@ -5,9 +5,12 @@ import android.view.View;
 import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentChange;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
 public class InviteActivity extends AppCompatActivity {
-
+    CollectionReference chat;
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
@@ -19,6 +22,14 @@ public class InviteActivity extends AppCompatActivity {
             public void onClick(View v) {
                 
             }
+        });
+        chat=FirebaseFirestore.getInstance().collection("chats").document("chats").collection("chats");
+
+        chat.addSnapshotListener((newChatSnapShot,error)->{
+            if(newChatSnapShot!=null&&newChatSnapShot.isEmpty()){
+                
+            }
+
         });
 
     }
