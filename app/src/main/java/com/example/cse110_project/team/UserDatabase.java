f23f036a28899ab37637d782ba297a63477c1019
Ray <ray.sakanoue@gmail.com>
2020-03-08 18:56:41 -0700

    starter for firebase messaging

--- app/src/main/java/com/example/cse110_project/team/UserDatabase.java
+++ app/src/main/java/com/example/cse110_project/team/UserDatabase.java
@@ -3,6 +3,6 @@ package com.example.cse110_project.team;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 
 public interface UserDatabase {
-    void addUser(GoogleSignInAccount account, String token);
+    void updateUser(GoogleSignInAccount account, String token);
     void addToTeam(String receiverEmail, String teamHostEmail);
 }
